19d12c982143afcdf1bb07313050b1bcd1cbd570
Rei-Tw <loic.kassel@gmail.com>
2025-04-12 15:27:11 +0000

    Setup GitLab CI/CD

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+commands=$(env | grep '^TW_'|sed 's/=/ /g; s/^TW_//')
+
+echo "${commands}" | tee env_generated.cfg
+
+if ! grep -qFx "exec env_generated.cfg" data/autoexec_server.cfg; then
+    echo "exec env_generated.cfg" >> data/autoexec_server.cfg
+fi
+
+sleep 2
+
+
+#if [ $(env | grep '^DEBUG=' | wc -l) -gt 0 ]
+#then
+#        echo "Starting blockworlds in debug mode..."
+#        gdb -ex run -ex bt --batch --args ./blockworlds_d -f data/autoexec_server.cfg
+#else
+echo "Starting blockworlds in release mode..."
+./blockworlds -f autoexec_server.cfg
+#fi
