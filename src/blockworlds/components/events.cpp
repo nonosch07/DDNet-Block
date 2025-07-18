ae28ae1acb1f52ce701a782a14b576457cddd034
Paul <paul.turkovskiy@gmail.com>
2025-07-18 17:44:06 +0300

    useless include

--- src/blockworlds/components/events.cpp
+++ src/blockworlds/components/events.cpp
@@ -10,7 +10,6 @@
 #include <blockworlds/components/core/component_registry.h>
 #include <blockworlds/components/events/event.h>
 #include <blockworlds/components/events/lmb.h>
-#include <blockworlds/components/promises.h>
 
 CEvents::CEvents(CGameContext *pGameServer) :
 	CComponent(pGameServer), m_pActiveEvent(nullptr), m_pEventToDelete(nullptr)
