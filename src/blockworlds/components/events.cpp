2011d3713e8a55535482d181da40ac7ae51160b5
qxdFox <jusi.p@icloud.com>
2026-03-04 18:54:29 +0100

    Don't register any chat commands in rcon

--- src/blockworlds/components/events.cpp
+++ src/blockworlds/components/events.cpp
@@ -69,8 +69,8 @@ void CEvents::OnDisable()
 	DEF("events_next_stage", "", CFGFLAG_SERVER | CFGFLAG_ANNOUNCE, ConEventsForceNextState, this, "Force current event to next stage") \
 	DEF("events_end", "", CFGFLAG_SERVER | CFGFLAG_ANNOUNCE, ConEventsForceEnd, this, "Forcefully end current event") \
 \
-	DEF("join", "", CFGFLAG_SERVER | CFGFLAG_CHAT | CFGFLAG_ANNOUNCE, ConJoin, this, "Chat command, try to register to current event") \
-	DEF("leave", "", CFGFLAG_SERVER | CFGFLAG_CHAT | CFGFLAG_ANNOUNCE, ConLeave, this, "Chat command, try to deregister from current event")
+	DEF("join", "", CFGFLAG_CHAT | CFGFLAG_ANNOUNCE, ConJoin, this, "Chat command, try to register to current event") \
+	DEF("leave", "", CFGFLAG_CHAT | CFGFLAG_ANNOUNCE, ConLeave, this, "Chat command, try to deregister from current event")
 
 void CEvents::OnConsoleInit()
 {
